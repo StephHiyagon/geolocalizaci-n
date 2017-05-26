e8ca548ebcfed16cd418ca2fb94b2e3ae80e5e44
Stephanie Hiyagon <mv.hiyagon@gmail.com>
2017-05-25 23:40:22 -0500

    ubica dos puntos y traza la ruta

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-  function initMap() {
+function initMap() {
   var laboratoriaLima = {lat: -12.1191427, lng: -77.0349046};
   var map = new google.maps.Map(document.getElementById("map"),{
     zoom: 18,
@@ -7,7 +7,8 @@
 
   var marcadorLaboratoria = new google.maps.Marker({
     position: laboratoriaLima,
-    map: map
+    map: map,
+    title:"Laboratoria-Lima"
   });
 
   function buscar() {
@@ -23,15 +24,48 @@
 		latitud = posicion.coords.latitude;
 		longitud = posicion.coords.longitude;
 
+    // var mapita2 = new google.maps.Map(document.getElementById("map"),{
+    //   zoom: 18,
+  	// 	center: {lat:latitud, lng:longitud}
+    // });
 		var miUbicacion = new google.maps.Marker({
 			position: {lat:latitud, lng:longitud},
 			animation: google.maps.Animation.DROP,
-			map: map
-		});
-		map.setZoom(18);
-		map.setCenter({lat:latitud, lng:longitud});
-	}
-	var error = function (error) {
-		alert("Tenemos un problema con encontrar tu ubicación");
-	}
+			map: map, // se usa mapita2 se crea un nuevo objeto map
+      title:"Usted esta aquí",
+      draggable:true,
+		  });
+        miUbicacion.setIcon('assets/images/icono1.png');
+		    map.setZoom(18);
+		    map.setCenter({lat:latitud, lng:longitud});
+
+        var objConfigDR={
+          map:map,
+          suppressMarkers:true
+        }
+
+        var objConfigDS={
+          origin:laboratoriaLima,
+          destination:miUbicacion.position,
+          travelMode:google.maps.TravelMode.DRIVING //puede ser WALKING, TRANSIT, BICYCLING
+        }
+
+        var dser= new google.maps.DirectionsService();//obtener coordenadas
+        var dren= new google.maps.DirectionsRenderer(objConfigDR);//traduce las coordenadas en ruta visible
+
+        dser.route(objConfigDS,funcionRutear);
+
+        function funcionRutear(resultados, status){
+          if(status=="OK"){
+            dren.setDirections(resultados);
+          }else{
+            alert("error" + status)
+          }
+        }
+	   }
+
+	  var error = function (error) {
+		    alert("Tenemos un problema con encontrar tu ubicación");
+	   }
+
 }
